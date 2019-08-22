0d5913a9bf8ce9584f2a495a9b14314411e3ea8a
unknown <dschenck@win.redhat.com>
2019-08-22 11:15:48 -0400

    correct code this time

--- setup/create-application-config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR=$(dirname "$0")
-
-
-echo "Creating reactiva-config config map"
-eval $(minishift oc-env)
-oc create configmap reactica-config --from-file=${SCRIPT_DIR}/application.yaml
